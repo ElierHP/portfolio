e2febbfeb97331df5afad5b8bc1e94c0b4f2db00
Elier H <elierporto@gmail.com>
2022-09-05 16:27:01 -0400

    Scss: 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,67 +1,23 @@
 import * as React from "react"
 import Button from "../components/Button"
 import Seo from "../components/Seo"
-import theme from "../theme"
-import styled from "@emotion/styled"
 import { Link } from "gatsby"
 import "normalize.css"
+import "../css/global.scss"
+import "../css/components/404.scss"
 
 const NotFoundPage = () => (
-  <Container>
+  <section className="error_page_container">
     <Seo title="404: Not found" />
-    <Title>404: Not Found</Title>
-    <Typography>You just hit a route that doesn&#39;t exist...</Typography>
-    <Typography>
+    <h1 className="center">404: Not Found</h1>
+    <p className="mt-0">You just hit a route that doesn&#39;t exist...</p>
+    <p className="mt-0">
       Press the Home button below to return to the main website.
-    </Typography>
-    <IconButton to="/">
-      <Button color={theme.colors.primary}>Home</Button>
-    </IconButton>
-  </Container>
+    </p>
+    <Link className="mt-2" to="/">
+      <Button color="bg-color_primary">Home</Button>
+    </Link>
+  </section>
 )
-
-//Styles
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  height: 100vh;
-  margin-top: -2rem;
-  max-width: 1250px;
-  padding: 0 2rem;
-  margin: auto;
-`
-
-const Title = styled.h1`
-  text-align: center;
-  font-size: ${theme.fontSizes.mobileHeading}rem;
-  ${theme.mq()[1]} {
-    font-size: ${theme.fontSizes.heading}rem;
-  }
-  font-family: ${theme.fonts.heading};
-  letter-spacing: ${theme.letterSpacing.heading}px;
-  line-height: ${theme.lineHeights.heading};
-`
-
-const Typography = styled.p`
-  text-align: center;
-  font-family: ${theme.fonts.body};
-  font-size: ${theme.fontSizes.medium}rem;
-  letter-spacing: ${theme.letterSpacing.body}px;
-  line-height: ${theme.lineHeights.body};
-  color: ${theme.colors.body};
-  margin-top: 0;
-`
-
-const IconButton = styled(Link)`
-  color: ${theme.colors.light};
-  text-decoration: none;
-  font-family: ${theme.fonts.body};
-  font-size: ${theme.fontSizes.medium}rem;
-  letter-spacing: ${theme.letterSpacing.body}px;
-  line-height: ${theme.lineHeights.body};
-  margin-top: 2rem;
-`
 
 export default NotFoundPage
